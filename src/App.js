6d9e1f97f8efa7f7f5e79ff376821c559b2d3f86
Mohammed Ashfaq <ashfaqm@conceptvines.com>
2025-04-07 13:36:09 +0530

    Updated all text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome!! to my second deplyment 
+          Welcome!! to my second deployment 
         </p>
         <a
           className="App-link"
